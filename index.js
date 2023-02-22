740f981d8fb21817ceecb2fd4447395b2c7b04b1
Saqib Ahmad <you@example.com>
2023-02-22 21:42:19 +0600

    server connected

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csyc5ob.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
+    const db = client.db("moon-tech");
     const productCollection = db.collection("product");
 
     app.get("/products", async (req, res) => {
